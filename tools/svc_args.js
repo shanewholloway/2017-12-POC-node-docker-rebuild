1246130c65682e5885d21e738f7d43594f368631
Shane Holloway <shane.holloway@ieee.org>
2017-12-20 14:23:30 -0700

    Simplify svc_args now that it is using devenv.service isolated configuration

--- tools/svc_args.js
+++ tools/svc_args.js
@@ -1,27 +1,13 @@
 const {dkrenv} = require('../package.json')
-
-const devenv_map = {
-  secrets: 'secret',
-  networks: 'network', ports: 'publish', dns: 'dns', 'dns-option': 'dns-option', 'dns-search': 'dns-search',
-  mount: 'mount', 'read-only': 'read-only',
-  env: 'env', user: 'user', group: 'group',
-  tty: 'tty', mode: 'mode', 'endpoint-mode': 'endpoint-mode',
-  replicas: 'replicas', constraints: 'constraint', 
-
-  'stop-signal': 'stop-signal', 'stop-grace-period': 'stop-grace-period',
-  'limit-cpu': 'limit-cpu', 'reserve-cpu': 'reserve-cpu', 'limit-memory': 'limit-memory', 'reserve-memory': 'reserve-memory',
-  'health-cmd': 'health-cmd', 'health-interval': 'health-interval', 'health-retries': 'health-retries', 'health-start-period': 'health-start-period', 'health-timeout': 'health-timeout',
-
-  'restart-condition': 'restart-condition', 'restart-delay': 'restart-delay', 'restart-window': 'restart-window', 'restart-max-attempts': 'restart-max-attempts',
-  'rollback-delay': 'rollback-delay', 'rollback-monitor': 'rollback-monitor', 'rollback-order': 'rollback-order', 'rollback-parallelism': 'rollback-parallelism', 'rollback-failure-action': 'rollback-failure-action', 'rollback-max-failure-ratio': 'rollback-max-failure-ratio',
-  'update-delay': 'update-delay', 'update-monitor': 'update-monitor', 'update-order': 'update-order', 'update-parallelism': 'update-parallelism', 'update-failure-action': 'update-failure-action', 'update-max-failure-ratio': 'update-max-failure-ratio',
-}
+const devenv_remap = { secrets: 'secret', networks: 'network', ports: 'publish' }
 
 function svc_args(service, join_str='\n') {
   const svc_args = [].concat(service._args_ || [])
-  for ( const [k,arg] of Object.entries(devenv_map) ) {
-    let lst = service[k]
+  for ( let [arg,lst] of Object.entries(service) ) {
     if (null == lst) continue
+
+    arg = devenv_remap[arg] || arg
+    if ('_' === arg[0]) continue
 
     if ('string' === typeof lst)
       lst = lst.split(/\w\+/)
